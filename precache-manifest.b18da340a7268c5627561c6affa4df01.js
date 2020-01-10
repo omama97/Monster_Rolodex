afe901fc0ddb5f3ed93beda363161680a8d9e82a
omama <oy8395@gmail,com>
2020-01-10 14:18:22 +0200

    Updates

---
+++ precache-manifest.b18da340a7268c5627561c6affa4df01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe52e987659a53465907fd46869f62ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882ccd3e74db508e23e2",
+    "url": "/static/css/main.daeefeae.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "882ccd3e74db508e23e2",
+    "url": "/static/js/main.36857eb2.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  }
+]);
